921ec522dc8af432d0c2e4c99aedbb9a925707bd
Eliot996 <math10t6@stud.kea.dk>
2021-11-17 11:12:34 +0100

    Added limit to how big numbers sum can accept

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -3,6 +3,9 @@ package com.company;
 public class Calculator {
 
     public int sum(int a, int b){
+        if (a > 100 ||b > 100){
+            throw new IllegalArgumentException("Can only accept numbers less than 100");
+        }
         return a + b;
     }
 
