11a71d681875324264394987d0f8392b0dee3fe4
Eliot996 <math10t6@stud.kea.dk>
2021-11-17 10:54:26 +0100

    Added additional test on different numbers pos/neg

--- test/com/company/CalculatorTest.java
+++ test/com/company/CalculatorTest.java
@@ -7,8 +7,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-    void sum() {
+    void testSum_withPositiveNumbers() {
         int result = new Calculator().sum(20,10);
         assertEquals(30, result);
+    }
+
+    @Test
+    void testSum_withNegativeNumbers() {
+        int result = new Calculator().sum(-2,-3);
+        assertEquals(-5, result);
+    }
+
+    @Test
+    void testSum_withPositiveNegativeNumber() {
+        int result = new Calculator().sum(20,-10);
+        assertEquals(10, result);
+    }
+    @Test
+    void testSum_withNegativePositiveNumber() {
+        int result = new Calculator().sum(-20,10);
+        assertEquals(-10, result);
     }
 }
